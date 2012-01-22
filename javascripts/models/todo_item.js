fc4aa2c2fb21cbf07c99184c9dca6afd53dffc80
David Strauß <mail@stravid.com>
2012-01-22 11:49:30 +0100

    Add comments.

--- javascripts/models/todo_item.js
+++ javascripts/models/todo_item.js
@@ -1,8 +1,16 @@
+/*
+  The model representing a todo item. It stores the data and
+  manages the editing process.
+*/
 var todoItemModel = function(text) {
+  // Make the attributes observable for the views
   this.text = ko.observable(text);
   this.isFinished = ko.observable(false);
   this.isEditing = ko.observable(false);
 
+  /*
+    Enables the editing mode if the todo item is not marked as finished.
+  */
   this.doubleClickHandler = function(element, event) {
     if (!this.isFinished()) {
        this.isEditing(true);
@@ -13,12 +21,19 @@ var todoItemModel = function(text) {
     this.save();
   };
 
+  /*
+    Checks if the user pressed the Enter key before calling the
+    save method.
+  */
   this.keyUpHandler = function(element, event) {
     if (event.keyCode == 13) {
       this.save();
     }
   };
 
+  /*
+    Disables the editing mode if the new text has a minimum length of 1 character.
+  */
   this.save = function() {
     if (this.text().length > 0) {
       this.isEditing(false);
--- javascripts/viewmodels/todo_list.js
+++ javascripts/viewmodels/todo_list.js
@@ -1,6 +1,17 @@
+/*
+  The View Model of the todo application.
+  It is responsible for offering data to the views
+  and removing / creating todo items.
+*/
 var todoListViewModel = {
+  // Make the attributes observable for the views
   inputText: ko.observable(""),
   todoItems: ko.observableArray(),
+
+  /*
+    Gets called on every keyup event of the input field
+    and checks if it is the Enter key.
+  */
   keyUpHandler: function(element, event) {
     if (event.keyCode == 13) {
       this.createTodoItem();
@@ -9,6 +20,11 @@ var todoListViewModel = {
   clickHandler: function(element, event) {
     this.createTodoItem();
   },
+
+  /* 
+    Creates a new todo item if the text has the minimum length of one
+    character. It also resets the input field.
+  */
   createTodoItem: function() {
     if (this.inputText().length > 0) {
       this.todoItems.push(new todoItemModel(this.inputText()));
